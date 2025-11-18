4c9865f62152a0b1d4874a3130c08b7933f7876d
ThanhBinh2006 <123310104+ThanhBinh2006@users.noreply.github.com>
2025-11-18 16:52:59 +0700

    update task_5
    Added functions for reading and writing JSON data, and optimized the main optimization logic to find the best marking and its value.

--- task5_optimization/optimization_solver.py
+++ task5_optimization/optimization_solver.py
@@ -16,3 +16,48 @@ Output:
 Author:
     Thanh Binh
 """
+import time
+import numpy as np
+import json
+def read(path):
+    f=open(path,"r")
+    arr=json.load(f)
+    f.close()
+    return arr
+def write(ans,path):
+    f=open(path,"w")
+    f.write(json.dumps(ans))
+    f.close()
+def optimization(markingArr,c):
+    ans=float("-inf")
+    ansArr=[]
+    for markingItem in markingArr:
+        tmp=0
+        i=0
+        for p in markingItem:
+            tmp=tmp+c[i]*markingItem[p]
+            i=i+1
+        if(tmp>ans):
+            ansArr.clear()
+            ans=tmp
+            ansArr.append(markingItem)
+        else:
+            if(tmp==ans): ansArr.append(markingItem)
+
+    return ans,ansArr
+def main():
+    markingArr=read("./data/reachable_markings.json")
+    #c=list(np.random.randn(len(markingArr[0])))
+    #write(c,"./data/CArr.json")
+    c=read("./data/CArr.json")
+    start=time.perf_counter()
+    bestVal,bestMarking=optimization(markingArr,c)
+    end=time.perf_counter()
+    ans={
+        "best_marking":bestMarking,
+        "max_value":bestVal,
+        "time_seconds:":end-start
+    }
+    write(ans,"./data/optimization_result.json")
+
+main()
